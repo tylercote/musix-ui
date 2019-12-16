454cd62daf4ba58bc0aab467a1d06c313015c33b
Tyler Cote <>
2019-12-16 13:04:01 -0500

    Remove token on unauthorization

--- src/utils/AxiosClient.js
+++ src/utils/AxiosClient.js
@@ -20,40 +20,42 @@ const axiosClient = () => {
     return config;
   });
 
-  // instance.interceptors.response.use(
-  //   (response) => {
-  //     return response;
-  //   },
-  //   function(error) {
-  //     console.log("AXIOS ERROR: ", error);
-  //     // const originalRequest = error.config;
-  //     if (error.response.status === 401) {
-  //       // axios
-  //       //   .post(
-  //       //     "http://localhost:8000/token-auth/",
-  //       //     {},
-  //       //     {
-  //       //       headers: {
-  //       //         "Content-Type": "application/json"
-  //       //       }
-  //       //     }
-  //       //   )
-  //       //   .then((res) => {
-  //       //     if (res.status === 200) {
-  //       //       localStorage.setItem("token", res.data.token);
-  //       //       localStorage.setItem("userId", res.data.user.id);
-  //       //       localStorage.setItem("username", res.data.user.username);
-  //       //       console.log("Change state 129");
-  //       //       this.setState({
-  //       //         loggedIn: true,
-  //       //         displayed_form: "",
-  //       //         username: res.data.user.username
-  //       //       });
-  //       //     }
-  //       //   });
-  //     }
-  //   }
-  // );
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    function(error) {
+      // const originalRequest = error.config;
+      if (error.response.status === 401) {
+        // axios
+        //   .post(
+        //     "http://localhost:8000/token-auth/",
+        //     {},
+        //     {
+        //       headers: {
+        //         "Content-Type": "application/json"
+        //       }
+        //     }
+        //   )
+        //   .then((res) => {
+        //     if (res.status === 200) {
+        //       localStorage.setItem("token", res.data.token);
+        //       localStorage.setItem("userId", res.data.user.id);
+        //       localStorage.setItem("username", res.data.user.username);
+        //       console.log("Change state 129");
+        //       this.setState({
+        //         loggedIn: true,
+        //         displayed_form: "",
+        //         username: res.data.user.username
+        //       });
+        //     }
+        //   });
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        localStorage.removeItem("username");
+      }
+    }
+  );
 
   return instance;
 };
